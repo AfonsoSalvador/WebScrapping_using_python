b3fffdf9a3b7be5739b77a540c4e1b5cb5ac7271
AfonsoSalvador <afonsosal032@gmail.com>
2022-07-12 13:06:20 -0300

    commit_05

--- webscrapping_test1.py
+++ webscrapping_test1.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-content = requests.get('https://ru.ufes.br/cardapio').text
+content = requests.get('https://ru.ufes.br/cardapio/2022-07-08').text
 
 soup = BeautifulSoup(content,'lxml')
 tags = soup.find_all('p')
@@ -29,11 +29,11 @@ if(len(tags)>3):
 
     while tags[i].text != "Salada":
         i +=1
-
+    i +=1
     SaladaA = tags[i].text
     i +=1
     while tags[i].text != "Prato Principal":
-        SaladaA = SaladaA + "\n "+tags[i].text
+        SaladaA = SaladaA + "\n"+tags[i].text
         i +=1
 
     i +=1
@@ -50,6 +50,10 @@ if(len(tags)>3):
 
     i=i+2
     SobremesaA = tags[i].text
+    i +=1
+    while tags[i].text != "*cardápio sujeito a alterações":
+        SobremesaA = SaladaA + "\n"+tags[i].text
+        i +=1
 
     print("Os pratos do almoço de hoje são:\n\nPara salada:\n",SaladaA,"\n\nPrato principal:\n",PratoPA, "\n\n Opção:\n",
     OpcaoA,"\n\nAcompanhamento:\n",AcompanhamentoA,"\n\nGaurnição:\n",GuarnicaoA,"\n\nSobremesa:\n",SobremesaA)
@@ -58,11 +62,11 @@ if(len(tags)>3):
     #Inicia leitura da Janta
         while tags[i].text != "Salada":
             i +=1
-
+        i +=1
         SaladaJ = tags[i].text
         i +=1
         while tags[i].text != "Prato Principal":
-            SaladaJ = SaladaJ + "\n "+tags[i].text
+            SaladaJ = SaladaJ + "\n"+tags[i].text
             i = i+1
 
         i=i+1
@@ -75,6 +79,10 @@ if(len(tags)>3):
         GuarnicaoJ = tags[i].text
         i=i+2
         SobremesaJ = tags[i].text
+        i +=1
+        while tags[i].text != "*cardápio sujeito a alterações":
+            SobremesaJ = SaladaJ + "\n"+tags[i].text
+            i +=1
 
         print("\n\n\nOs pratos da Janta de hoje são:\n\nPara salada:\n",SaladaJ,"\n\nPrato principal:\n",PratoPJ, "\n\n Opção:\n",
         OpcaoJ,"\n\nAcompanhamento:\n",AcompanhamentoJ,"\n\nGaurnição:\n",GuarnicaoJ,"\n\nSobremesa:\n",SobremesaJ)
