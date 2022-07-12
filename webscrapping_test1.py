efed5079b38e95ca024217ab43213aa995cd304e
AfonsoSalvador <afonsosal032@gmail.com>
2022-07-12 19:03:55 -0300

    commit-06

--- webscrapping_test1.py
+++ webscrapping_test1.py
@@ -2,10 +2,12 @@ from random import gauss
 from bs4 import BeautifulSoup
 import requests
 
+
 content = requests.get('https://ru.ufes.br/cardapio').text
 
-soup = BeautifulSoup(content, 'lxml')
+soup = BeautifulSoup(content,'lxml')
 tags = soup.find_all('p')
+
 
 SaladaA = []
 PratoPA = []
@@ -21,46 +23,69 @@ AcompanhamentoJ = []
 GuarnicaoJ = []
 SobremesaJ = []
 
-i=4
 
-SaladaA = tags[3].text
-while tags[i].text != "Prato Principal":
-    SaladaA = SaladaA + "\n "+tags[i].text
-    i = i+1
+if(len(tags)>3):
+    i=0
 
-i=i+1
-PratoPA = tags[i].text
-i=i+2
-OpcaoA = tags[i].text
-i=i+2
-AcompanhamentoA = tags[i].text
-i=i+2
-GuarnicaoA = tags[i].text
-i=i+2
-SobremesaA = tags[i].text
+    while tags[i].text != "Salada":
+        i +=1
+    i +=1
+    SaladaA = tags[i].text
+    i +=1
+    while tags[i].text != "Prato Principal":
+        SaladaA = SaladaA + "\n"+tags[i].text
+        i +=1
 
+    i +=1
+    PratoPA = tags[i].text
 
-#Inicia leitura da Janta
-i = i+5
-SaladaJ = tags[i-1].text
-while tags[i].text != "Prato Principal":
-    SaladaJ = SaladaJ + "\n "+tags[i].text
-    i = i+1
+    i +=2
+    OpcaoA = tags[i].text
 
-i=i+1
-PratoPJ = tags[i].text
-i=i+2
-OpcaoJ = tags[i].text
-i=i+2
-AcompanhamentoJ = tags[i].text
-i=i+2
-GuarnicaoJ = tags[i].text
-i=i+2
-SobremesaJ = tags[i].text
+    i=i+2
+    AcompanhamentoA = tags[i].text
 
-print("Os pratos do almoço de hoje são:\n\nPara salada:\n",SaladaA,"\n\nPrato principal:\n",PratoPA, "\n\n Opção:\n",
-OpcaoA,"\n\nAcompanhamento:\n",AcompanhamentoA,"\n\nGaurnição:\n",GuarnicaoA,"\n\nSobremesa:\n",SobremesaA)
+    i=i+2
+    GuarnicaoA = tags[i].text
 
+    i=i+2
+    SobremesaA = tags[i].text
+    i +=1
+    while tags[i].text != "*cardápio sujeito a alterações":
+        SobremesaA = SaladaA + "\n"+tags[i].text
+        i +=1
 
-print("\n\n\nOs pratos da Janta de hoje são:\n\nPara salada:\n",SaladaJ,"\n\nPrato principal:\n",PratoPJ, "\n\n Opção:\n",
-OpcaoJ,"\n\nAcompanhamento:\n",AcompanhamentoJ,"\n\nGaurnição:\n",GuarnicaoJ,"\n\nSobremesa:\n",SobremesaJ)
+    print("Os pratos do almoço de hoje são:\n\nPara salada:\n",SaladaA,"\n\nPrato principal:\n",PratoPA, "\n\n Opção:\n",
+    OpcaoA,"\n\nAcompanhamento:\n",AcompanhamentoA,"\n\nGaurnição:\n",GuarnicaoA,"\n\nSobremesa:\n",SobremesaA)
+
+    if(len(tags)>20):
+    #Inicia leitura da Janta
+        while tags[i].text != "Salada":
+            i +=1
+        i +=1
+        SaladaJ = tags[i].text
+        i +=1
+        while tags[i].text != "Prato Principal":
+            SaladaJ = SaladaJ + "\n"+tags[i].text
+            i = i+1
+
+        i=i+1
+        PratoPJ = tags[i].text
+        i=i+2
+        OpcaoJ = tags[i].text
+        i=i+2
+        AcompanhamentoJ = tags[i].text
+        i=i+2
+        GuarnicaoJ = tags[i].text
+        i=i+2
+        SobremesaJ = tags[i].text
+        i +=1
+        while tags[i].text != "*cardápio sujeito a alterações":
+            SobremesaJ = SaladaJ + "\n"+tags[i].text
+            i +=1
+
+        print("\n\n\nOs pratos da Janta de hoje são:\n\nPara salada:\n",SaladaJ,"\n\nPrato principal:\n",PratoPJ, "\n\n Opção:\n",
+        OpcaoJ,"\n\nAcompanhamento:\n",AcompanhamentoJ,"\n\nGaurnição:\n",GuarnicaoJ,"\n\nSobremesa:\n",SobremesaJ)
+
+else:
+    print("Ainda não foi informada as refeicões de hoje!\n")
